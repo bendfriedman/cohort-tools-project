7636559bff39797794ad51de8ebb2d01cf05fca4
Carolina <cisj24@gmail.com>
2024-03-26 15:29:21 +0100

    added cohort model

--- server/app.js
+++ server/app.js
@@ -7,10 +7,8 @@ const PORT = 5005;
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -21,14 +19,12 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
-
 
 // START SERVER
 app.listen(PORT, () => {
